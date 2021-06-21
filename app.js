ce17ed057d5064fe64b4c0e06ba445ad885fb603
APet99 <Petersonalex99@gmail.com>
2021-06-21 01:27:06 -0700

    Basic CRUD implemented

--- app.js
+++ app.js
@@ -5,17 +5,50 @@ const logger = require('morgan');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-const savesRouter = require('./routes/saves');
+const Auth = require('./models/Auth');
 
+const savesRouter = require('./routes/saves');
+const authRouter = require('./routes/auth');
 
 const app = express();
 
+
+function authentication(req, res, next) {
+    const authMethod = 'Basic';
+    const authheader = req.headers.authorization.split(" ");
+
+    if (!authheader) {
+        const err = new Error('Not Authenticated!');
+        res.setHeader('authorization', authMethod);
+        err.status = 401;
+        return next(err)
+    }
+
+
+    Auth.countDocuments({'auth': authheader[1]}, function (err, count) {
+        if (count > 0 && authheader[0] === authMethod) {
+            next();
+        }else{
+            const err = new Error('Not Authenticated!');
+            res.setHeader('authorization', authMethod);
+            err.status = 401;
+            return next(err);
+        }
+    });next
+
+
+
+}
+app.use(authentication)
+
+/*
+* Creates a Mongo connection string
+* */
 function getDBConnectionString(){
-    return `mongodb+srv://${encodeURIComponent(process.env.DB_USER)}:${encodeURIComponent(process.env.DB_PASS)}@cluster0.cfgby.mongodb.net/${encodeURIComponent(process.env.DB_Name_Saves)}?retryWrites=true&w=majority`;
+    return `mongodb+srv://${encodeURIComponent(process.env.DB_USER)}:${encodeURIComponent(process.env.DB_PASS)}@${encodeURIComponent(process.env.DB_LOCATION)}/${encodeURIComponent(process.env.DB_Name_Saves)}?retryWrites=true&w=majority`;
 }
 
 const dbURI = getDBConnectionString();
-console.log(dbURI);
 
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(result => app.listen(3001, (req, res) => console.log('Connected to port 3001')))
@@ -23,14 +56,14 @@ mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
 
 
 
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb', extended: true}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/saves', savesRouter);
+app.use('/auth', authRouter);
 
 module.exports = app;
--- routes/saves.js
+++ routes/saves.js
@@ -1,28 +1,282 @@
 const express = require('express');
 const router = express.Router();
-const Save = require("../models/Save");
+const mongoose = require('mongoose');
+const saves = mongoose.connection.collection('saves');
 
 
-router.get('/', function(req, res, next) {
-    res.send('HelloWorld');
+router.get('/', async (req, res, next) => {
+    res.status(200).send({'message': 'Hello World'})
 });
 
 
+//todo Is steam usernames case sensative?
+async function doesSaveExist(steamName = '', steamID = '') {
+    if (!steamName && !steamID) {
+        throw new Error('Steam Name or Steam ID not provided');
+    }
+    // check if a save exists where name or ID is found
+    let query = {}
 
-router.route('/addSave').post(async (req, res) =>{
-    const saveEntry = new Save({
-        userName : 'testUser',
-        objects : ['Object1', 'Object2']
-    });
-    saveEntry.save()
-        .then((result) => {
+    if (steamName) {
+        query.Nickname = `${steamName}`;
+    }
+    if (steamID) {
+        query.Description = {'$regex': `${steamID}`};
+    }
 
-            res.send(result);
-            res.status(200);
-        })
-        .catch((err) => {
-            console.log(err);
+    const numDoc = await saves.countDocuments(query);
+
+    return numDoc > 0;
+}
+
+async function insertUser(json) {
+    return await saves.insertOne(json)
+}
+
+async function getByName(steamName) {
+    try {
+        return await saves.findOne({'Nickname': steamName});
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+async function getByID(steamID) {
+    try {
+        return await saves.findOne({'Description': {'$regex': steamID}});
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+
+/*CREATE*/
+router.route('/add').post(async (req, res, next) => {
+    const bodyData = req.body;
+    try {
+        res.status(200).send(await insertUser(bodyData));
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/addMultiple').post(async (req, res, next) => {
+    const data = (req.body);
+    try {
+        for (let saveBox of data.ObjectStates[0].ContainedObjects) {
+            await insertUser(saveBox);
+        }
+        res.status(200).send(data.ObjectStates[0].ContainedObjects);
+    } catch (e) {
+        next(e);
+    }
+});
+
+
+/*READ*/
+router.route('/getByName').get(async (req, res, next) => {
+    try {
+        const steamName = req.query.steamName;
+        if (!steamName) {
+            throw new Error('Steam Name not provided');
+        }
+        res.send(await getByName(steamName));
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/getByID').get(async (req, res, next) => {
+    try {
+        const steamID = req.query.steamID;
+        if (!steamID) {
+            throw new Error('Steam ID not provided');
+        }
+        res.send(await getByID(steamID));
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/get').get(async (req, res, next) => {
+    try {
+        const steamName = req.query.steamName;
+        const steamID = req.query.steamID;
+        if (!steamName && !steamID) {
+            throw new Error('Steam Name or Steam ID not provided');
+        }
+        // check if a save exists where name or ID is found
+        let query = {}
+
+        if (steamName) {
+            query.Nickname = `${steamName}`;
+        }
+        if (steamID) {
+            query.Description = {'$regex': `${steamID}`};
+        }
+
+        res.send(await saves.findOne(query));
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/getMultipleByName').get(async (req, res, next) => {
+    try {
+        let result = [];
+
+        const userNames = JSON.parse(req.query.userNames);
+        for (const name of userNames) {
+            let instance = await saves.findOne({'Nickname': name})
+            if (instance) {
+                result.push(instance);
+            }
+        }
+        res.status(200).send(result);
+    } catch
+        (e) {
+        next(e);
+    }
+});
+
+router.route('/getAll').get(async (req, res, next) => {
+    try {
+        await saves.find({}).toArray(function (e, documents) {
+            if (e) next(e);
+
+            res.send(documents);
         });
+    } catch (e) {
+        next(e);
+    }
+});
+
+// todo Get al saves where (query)
+// todo Get all saves where not (query)
+
+/*UPDATE*/
+// Update an existing save's changes.
+router.route('/updateName').put(async (req, res, next) => {
+    try {
+        const name = req.query.steamName;
+        const newName = req.query.newName;
+        if (getByName(name)) {
+            res.send(await saves.findOneAndUpdate({'Nickname': name}, {'$set': {'Nickname': newName}}, {upsert: false}));
+        }
+    } catch (e) {
+        next(e);
+    }
+});
+
+// todo update last plated??
+
+router.route('/replaceByName').put(async (req, res, next) => {
+    try {
+        const name = req.query.steamName;
+        if (req.body._id) {
+            delete req.body._id;
+        }
+        if (getByName(name)) {
+            res.send(await saves.findOneAndReplace({'Nickname': name}, req.body));
+        }
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/replaceByID').put(async (req, res, next) => {
+    try {
+        const id = req.query.steamID;
+        if (req.body._id) {
+            delete req.body._id;
+        }
+        if (getByID(id)) {
+            res.send(await saves.findOneAndReplace({'Description': {'$regex': id}}, req.body));
+        }
+    } catch (e) {
+        next(e);
+    }
+});
+
+
+// Update All Saves
+// router.route('/addFieldToAll').put(async (req, res, next) => {
+//     try {
+//         const fieldName = req.query.fieldName;
+//         const value = req.query.fieldValue;
+//
+//         res.status(202).send(await saves.updateMany({}, {fieldName: value}, {upsert: true}))
+//     } catch (e) {
+//         next(e);
+//     }
+// })
+//
+// router.route('/updateFieldForAll').put(async (req, res, next) => {
+//     try {
+//         const fieldName = req.query.fieldName;
+//         const value = req.query.fieldValue;
+//
+//         res.status(202).send(await saves.updateMany({},{fieldName: value}, {upsert: false}))
+//     } catch (e) {
+//         next(e);
+//     }
+// })
+
+// todo Update Saves Where (query)
+
+/*DELETE*/
+router.route('/deleteByName').delete(async (req, res, next) => {
+    try {
+        const steamName = req.query.steamName
+        await saves.deleteOne({'Nickname': steamName});
+        res.status(204).send();
+
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/deleteByID').delete(async (req, res, next) => {
+    try {
+        const steamID = req.query.steamID
+        await saves.deleteOne({'Description': {'$regex': steamID}});
+        res.status(204).send();
+
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.route('/deleteAll').delete(async (req, res, next) => {
+    try {
+        const confirmation = req.query.confirmation;
+        if (!confirmation) {
+            next('You must confirm to delete all saves');
+        }
+        if (confirmation === 'ConfirmDeleteAll') {
+            await saves.deleteMany({});
+            res.status(204).send();
+        } else {
+            next('Confirmation does not match');
+        }
+    } catch (e) {
+        next(e);
+    }
+});
+
+// todo Delete all saves where (query)
+
+router.route('/doesSaveExist').get(async (req, res) => {
+    const data = req.query;
+    const steamName = data.steamName;
+    const steamID = data.steamID
+
+    try {
+        const result = await doesSaveExist(steamName, steamID);
+        res.status(200).send(result);
+    } catch (e) {
+        res.status(400).send();
+    }
 });
 
 
